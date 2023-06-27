07bc69418c5b61b09b0b30870619ea9371e4039d
akshay.mandavkar <akshay.mandavkar@inbl01lap7058.brd.g-c-i.com>
2023-06-27 19:49:14 +0530

    !st commit

---
+++ Test Suites/TS01.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>29b9ee49-14ab-4e76-889c-7e75d1de1dc9</testSuiteGuid>
+</TestSuiteEntity>
